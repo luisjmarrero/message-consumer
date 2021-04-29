8094cd2375994f24f11e1c3a43097fe912e7692c
Luis Marrero <luis124ma@gmail.com>
2021-04-28 23:47:08 -0400

    commit

--- src/main/java/com/example/messageconsumer/MessagesConsumer.java
+++ src/main/java/com/example/messageconsumer/MessagesConsumer.java
@@ -1,6 +1,7 @@
 package com.example.messageconsumer;
 
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Log4j2
 @Getter
 public class MessagesConsumer {
     private final List<String> messages = new ArrayList<>();
@@ -15,7 +17,7 @@ public class MessagesConsumer {
     @KafkaListener(topics = "messages", groupId = "kafka-sandbox")
     public void listen(String message) {
         synchronized (messages) {
-            System.out.println("Received message: " + message);
+            log.info("Received message: {}", message);
             messages.add(message);
         }
     }
